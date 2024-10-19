842c0a24866d99feb03ac0199f91b06e9f3682a2
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-10-19 12:09:42 +0600

    memorable-water-bottle project updated

--- src/components/bottle/Bottle.jsx
+++ src/components/bottle/Bottle.jsx
@@ -1,14 +1,20 @@
+import PropTypes from 'prop-types'
 import './bottle.css'
-const Bottle = ({bottle}) => {
-    console.log(bottle)
+const Bottle = ({bottle, handleAddToCard}) => {
+    // console.log(bottle)
     const {name, price, img} = bottle;
     return (
         <div className="bottle">
             <h3>Name: {name}</h3>
             <img src={img} alt="" />
             <p>Price: ${price}</p>
+            <button onClick={() => handleAddToCard(bottle)}>Purchase</button>
         </div>
     );
 };
 
+Bottle.propTypes = {
+    bottle: PropTypes.object.isRequired,
+    handleAddToCard: PropTypes.func.isRequired
+}
 export default Bottle;
--- src/components/bottles/Bottles.jsx
+++ src/components/bottles/Bottles.jsx
@@ -1,20 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import Bottle from '../bottle/Bottle';
 import './Bottles.css'
+import { addToLS, getStoredCart, removeFromLS } from '../utilities/localstorage';
+import Cart from '../Cart/Cart';
+
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
-
+    const [cart, setCart] = useState([]);
     useEffect(() =>{
         fetch('bottles.json')
         .then(res => res.json())
         .then(data => setBottles(data))
     } ,[])
+
+    //load cart from local storage
+    useEffect(() => {
+        console.log('Called the use effect', bottles.length);
+        if(bottles.length > 0){
+            const storedCart = getStoredCart();
+            console.log(storedCart, bottles);
+            
+            const saveCart = [];
+            for(const id of storedCart){
+                console.log(id);
+                const bottle = bottles.find(bottle => bottle.id === id);
+                if(bottle){
+                    saveCart.push(bottle);
+                }
+            }
+            console.log(saveCart);
+            setCart(saveCart);
+        }
+        
+    }, [bottles])
+
+    const handleAddToCard = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+        addToLS(bottle.id);
+    }
+    const handleRemoveFromCart = id =>{
+        // visual cart remove
+        const remainingCart = cart.filter(bottle => bottle.id !== id);
+        setCart(remainingCart);
+        // store LS
+        removeFromLS(id);
+    }
+
     return (
         <div>
             <h2>Bottles Here: {bottles.length}</h2>
+            <Cart cart={cart} handleRemoveFromCart = {handleRemoveFromCart}></Cart>
             <div className='bottles-container'>
                 {
-                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                    bottles.map(bottle => <Bottle 
+                        key={bottle.id} bottle={bottle}
+                        handleAddToCard = {handleAddToCard}
+                    ></Bottle>)
                 }
             </div>
         </div>
